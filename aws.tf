1002bc3039a97dc4a68db7513e4ec20203238549
Lance Haig <lnhaig@gmail.com>
2019-10-08 10:13:10 +0200

    Link All the parts together

--- aws.tf
+++ aws.tf
@@ -26,3 +26,53 @@ resource "aws_route53_record" "aws_sub_zone_ns" {
     awsns
   ]
 }
+
+# GCP SubZone
+
+resource "aws_route53_zone" "gcp_sub_zone" {
+ name          = "${var.namespace}.gcp.${var.hosted-zone}"
+ comment       = "Managed by Terraform, Delegated Sub Zone for GCP for  ${var.namespace}"
+ force_destroy = false
+ tags = {
+    name           = var.namespace
+    owner          = var.owner
+    created-by     = var.created-by
+ }
+}
+
+ resource "aws_route53_record" "gcp_sub_zone" {
+   zone_id = "${data.aws_route53_zone.main.zone_id}"
+   name    = "${var.namespace}.gcp.${var.hosted-zone}"
+   type    = "NS"
+   ttl     = "30"
+
+   records = [ 
+     for gcpns in google_dns_managed_zone.gcp_sub_zone.name_servers:
+     gcpns
+    ]
+ }
+
+# Azure SUBZONE 
+
+resource "aws_route53_zone" "azure_sub_zone" {
+  name = "${var.namespace}.azure.${var.hosted-zone}"
+  comment = "Managed by Terraform, Delegated Sub Zone for Azure for ${var.namespace}"
+
+  tags = {
+    name        = var.namespace
+    owner       = var.owner
+    created-by  = var.created-by
+  }
+}
+
+resource "aws_route53_record" "azure_sub_zone_ns" {
+  zone_id = "${data.aws_route53_zone.main.zone_id}"
+  name    = "${var.namespace}.azure.${var.hosted-zone}"
+  type    = "NS"
+  ttl     = "30"
+
+  records = [
+    for azurens in azurerm_dns_zone.azure_sub_zone.name_servers:
+    azurens
+  ]
+}
