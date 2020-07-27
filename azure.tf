8d7dce945f86b528d5f1b9d071aae88b76a2b3ba
Yulei Liu <yulei.liu@gmail.com>
2020-07-27 14:57:52 +1000

    more 0.12 syntax

--- azure.tf
+++ azure.tf
@@ -9,7 +9,7 @@ resource "azurerm_resource_group" "dns_resource_group" {
 resource "azurerm_dns_zone" "azure_sub_zone" {
   count = var.create_azure_dns_zone ? 1 : 0
   name                = "${var.namespace}.azure.${var.hosted-zone}"
-  resource_group_name = "${azurerm_resource_group.dns_resource_group.0.name}"
+  resource_group_name = azurerm_resource_group.dns_resource_group.0.name
   tags = {
     name        = var.namespace
     owner       = var.owner
