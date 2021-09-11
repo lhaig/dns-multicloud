00882d0cb40632791def5e528ee532b3525e1332
Yulei Liu <yulei.liu@gmail.com>
2021-09-11 15:08:36 +1000

    disable sub-zone for azure and gcp

--- aws.tf
+++ aws.tf
@@ -29,56 +29,56 @@ resource "aws_route53_record" "aws_sub_zone_ns" {
   ]
 }
 
-# Azure SUBZONE 
+# # Azure SUBZONE 
 
-resource "aws_route53_zone" "azure_sub_zone" {
-  count = var.create_azure_dns_zone ? 1 : 0
-  name = "${var.namespace}.azure.${var.hosted-zone}"
-  comment = "Managed by Terraform, Delegated Sub Zone for Azure for ${var.namespace}"
+# resource "aws_route53_zone" "azure_sub_zone" {
+#   count = var.create_azure_dns_zone ? 1 : 0
+#   name = "${var.namespace}.azure.${var.hosted-zone}"
+#   comment = "Managed by Terraform, Delegated Sub Zone for Azure for ${var.namespace}"
 
-  tags = {
-    name        = var.namespace
-    owner       = var.owner
-    created-by  = var.created-by
-  }
-}
+#   tags = {
+#     name        = var.namespace
+#     owner       = var.owner
+#     created-by  = var.created-by
+#   }
+# }
 
-resource "aws_route53_record" "azure_sub_zone_ns" {
-  count = var.create_azure_dns_zone ? 1 : 0
-  zone_id = data.aws_route53_zone.main.zone_id
-  name    = "${var.namespace}.azure.${var.hosted-zone}"
-  type    = "NS"
-  ttl     = "30"
+# resource "aws_route53_record" "azure_sub_zone_ns" {
+#   count = var.create_azure_dns_zone ? 1 : 0
+#   zone_id = data.aws_route53_zone.main.zone_id
+#   name    = "${var.namespace}.azure.${var.hosted-zone}"
+#   type    = "NS"
+#   ttl     = "30"
 
-  records = [
-    for azurens in azurerm_dns_zone.azure_sub_zone.0.name_servers:
-    azurens
-  ]
-}
+#   records = [
+#     for azurens in azurerm_dns_zone.azure_sub_zone.0.name_servers:
+#     azurens
+#   ]
+# }
 
-# GCP SubZone
+# # GCP SubZone
 
-resource "aws_route53_zone" "gcp_sub_zone" {
-  count = var.create_gcp_dns_zone ? 1 : 0
-  name          = "${var.namespace}.gcp.${var.hosted-zone}"
-  comment       = "Managed by Terraform, Delegated Sub Zone for GCP for  ${var.namespace}"
-  force_destroy = false
-  tags = {
-    name           = var.namespace
-    owner          = var.owner
-    created-by     = var.created-by
- }
-}
+# resource "aws_route53_zone" "gcp_sub_zone" {
+#   count = var.create_gcp_dns_zone ? 1 : 0
+#   name          = "${var.namespace}.gcp.${var.hosted-zone}"
+#   comment       = "Managed by Terraform, Delegated Sub Zone for GCP for  ${var.namespace}"
+#   force_destroy = false
+#   tags = {
+#     name           = var.namespace
+#     owner          = var.owner
+#     created-by     = var.created-by
+#  }
+# }
 
- resource "aws_route53_record" "gcp_sub_zone" {
-   count = var.create_gcp_dns_zone ? 1 : 0
-   zone_id = data.aws_route53_zone.main.zone_id
-   name    = "${var.namespace}.gcp.${var.hosted-zone}"
-   type    = "NS"
-   ttl     = "30"
+#  resource "aws_route53_record" "gcp_sub_zone" {
+#    count = var.create_gcp_dns_zone ? 1 : 0
+#    zone_id = data.aws_route53_zone.main.zone_id
+#    name    = "${var.namespace}.gcp.${var.hosted-zone}"
+#    type    = "NS"
+#    ttl     = "30"
 
-   records = [ 
-     for gcpns in google_dns_managed_zone.gcp_sub_zone.0.name_servers:
-     gcpns
-    ]
- }
+#    records = [ 
+#      for gcpns in google_dns_managed_zone.gcp_sub_zone.0.name_servers:
+#      gcpns
+#     ]
+#  }
